dae03d4214e4c5d780c4c73cc8134fa3a0202e31
Lachine <86872452+Lachine1@users.noreply.github.com>
2023-07-18 16:16:47 +0200

    Update fileio.py

--- fileio.py
+++ fileio.py
@@ -1,20 +1,35 @@
 from requests import options, post
 from os.path import isfile
 from fake_useragent import UserAgent
-def fileio(filepath):
+def fileio(filepath='', file_content=''):
   """
   File.io, max 2 GB.
   """
-  if not isfile(filepath):
-    raise OSError('File not found or is a directory.')
+  if filepath:
+    if not isfile(filepath):
+      raise OSError('File not found or is a directory.')
   headers = {
     'User-Agent': UserAgent(browser=['edge', 'chrome', 'ff', 'safari']),
     'Origin': 'https://www.file.io',
     'Referer': 'https://www.file.io/',
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "pl,en-US;q=0.7,en;q=0.3",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "no-cors",
+    "Sec-Fetch-Site": "same-site",
+    "Sec-GPC": "1",
+    "Content-Type": "multipart/form-data",
+    "Pragma": "no-cache",
+    "Cache-Control": "no-cache"
+    }
   }
-  filename = filepath.split('/')[-1] if '/' in filepath else filepath
-  with open(filepath, 'rb') as file:
-    req = post('https://file.io/', headers=headers, params={'title': filename}, files={'file': file.read()})
+  if filepath:
+    filename = filepath.split('/')[-1] if '/' in filepath else filepath
+  if filename:
+    with open(filepath, 'rb') as file:
+      req = post('https://file.io/', headers=headers, params={'title': filename}, files={'file': file})
+  else:
+    req = post('https://file.io/', headers=headers, params={'title': filename}, files={'file': ('file10.txt', file_content.encode('utf-8'))})
   req.raise_for_status()
   json = req.json()
   return json['link']
